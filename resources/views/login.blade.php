fb601bdfcbc611d31d724cc05a4925b9498daf03
AngelRamirez230 <angelramirezperez24@gmail.com>
2025-12-01 12:14:10 -0600

    Modificación del login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <h1>Hola este va a ser el login de IDMA</h1>
+
+    <p>Este es mi primer archivo Blade.</p>
+</body>
+</html>
