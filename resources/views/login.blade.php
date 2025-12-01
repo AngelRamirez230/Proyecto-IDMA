ae2a7b847387bc3710f8536cc357ced3e5f0da26
AngelRamirez230 <angelramirezperez24@gmail.com>
2025-12-01 14:16:57 -0600

    Inicio de sesión
    Primera versión

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,11 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <title>Iniciar sesión</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    @vite(['resources/css/login.css'])
 </head>
 <body>
-    <h1>Hola este va a ser el login de IDMA</h1>
+    
+    <div class="rectanguloAmarillo">
+        <form class="formularioLogin">
 
-    <p>Este es mi primer archivo Blade.</p>
-    <p>Este es mi primer archivo Blade.</p>
+            <!-- Input usuario con icono -->
+            <div class="inputGroup">
+                <i class="fa-solid fa-user iconoInput"></i>
+                <input type="text" id="usuario" name="usuario" placeholder="Matrícula o correo">
+            </div>
+
+            <!-- Input contraseña con icono -->
+            <div class="inputGroup">
+                <i class="fa-solid fa-lock iconoInput"></i>
+                <input type="password" id="password" name="password" placeholder="Contraseña">
+            </div>
+
+            <!-- Botones -->
+            <button type="submit" class="btnLogin">Iniciar sesión</button>
+            <button type="button" class="btnLogin">Google</button>
+
+        </form>
+    </div>
+
+    <img src="{{ asset('imagenes/LOGO_INTERIOR_IDMA.png') }}" class="imagenIDMA" alt="imagenidma">
+    <img src="{{ asset('imagenes/ImagenDeFondo.png') }}" class="fondoSuperio" alt="imagenSuperiorFondo">
+    
+    <div class="rectanguloRojoFondoLogin"></div>
+
+    
 </body>
 </html>
