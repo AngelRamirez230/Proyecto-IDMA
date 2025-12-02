0730bbf8ea1a701560b47613d1890b98bf440bc6
AngelRamirez230 <angelramirezperez24@gmail.com>
2025-12-02 11:51:16 -0600

    Login
    Actualización de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <title>Iniciar sesión</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    @vite(['resources/css/login.css'])
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+    <img src="{{ asset('imagenes/ImagenDeFondo.png') }}" class="fondoSuperior" alt="imagenSuperiorFondo">
     
+    <div class="rectanguloRojoFondoLogin"></div>
+
     <div class="rectanguloAmarillo">
+        <img src="{{ asset('imagenes/LOGO_INTERIOR_IDMA.png') }}" class="imagenIDMA" alt="imagenidma">
+        
         <form class="formularioLogin">
+            
 
             <div class="cajaDeTexto">
                 <i class="fa-solid fa-circle-user iconoInput"></i>
@@ -37,10 +43,6 @@
         </form>
     </div>
 
-    <img src="{{ asset('imagenes/LOGO_INTERIOR_IDMA.png') }}" class="imagenIDMA" alt="imagenidma">
-    <img src="{{ asset('imagenes/ImagenDeFondo.png') }}" class="fondoSuperior" alt="imagenSuperiorFondo">
-    
-    <div class="rectanguloRojoFondoLogin"></div>
 
 </body>
 </html>
