df75ddd7d95b2648e100de0782db704d181bbe51
AngelRamirez230 <angelramirezperez24@gmail.com>
2026-01-27 16:42:48 -0600

    Fechas del reporte de finanzas

--- resources/views/SGFIDMA/moduloReportesFinanzas/eleccionDefechas.blade.php
+++ resources/views/SGFIDMA/moduloReportesFinanzas/eleccionDefechas.blade.php
@@ -9,23 +9,25 @@
 <body>
     @include('layouts.barraNavegacion')
 
-    <form action="#" method="POST" class="formulario">
+    <form action="#" method="POST" class="formulario2">
     @csrf
 
-        <div class="form-group">
+        <h1 class="titulo-form2">Elige las fechas para el reporte</h1>
+
+        <div class="form-group2">
             <label for="fechaInicioReporte">Fecha de incio del reporte:</label>
-            <input type="date" id="fechaInicioReporte" name="fechaInicioReporte" max="{{ date('Y-m-d') }}" class="input-date" required>
+            <input type="date" id="fechaInicioReporte" name="fechaInicioReporte" max="{{ date('Y-m-d') }}" class="input-chico2" required>
         </div>
 
-        <div class="form-group">
+        <div class="form-group2">
             <label for="fechaFinalReporte">Fecha final del reporte:</label>
-            <input type="date" id="fechaFinalReporte" name="fechaFinalReporte" max="{{ date('Y-m-d') }}" class="input-date" required>
+            <input type="date" id="fechaFinalReporte" name="fechaFinalReporte" max="{{ date('Y-m-d') }}" class="input-chico2" required>
         </div>
 
 
-        <div class="form-group">
-            <button type="submit" class="btn-boton-formulario">Generar reporte</button>
-            <a href="{{ route('apartadoReportesFinanzas')}}" class="btn-boton-formulario btn-cancelar">Cancelar</a>
+        <div class="form-group2">
+            <button type="submit" class="btn-boton-formulario2">Generar reporte</button>
+            <a href="{{ route('apartadoReportesFinanzas')}}" class="btn-boton-formulario2 btn-cancelar2">Cancelar</a>
         </div>
     </form>
 
